036639e41bdf121ae17e4576167b76b6d7908da9
devendra-dantal04 <devendra.dantal04@gmail.com>
2022-12-25 15:17:33 +0530

    Image Generator using OpenAI Frontend

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 
 //Enable BodyParser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openai.routes'));
 
---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+const imageForm = document.querySelector('#image-form');
+
+const onSubmit = (e) => {
+    e.preventDefault();
+
+    document.querySelector('.msg').textContent = "";
+    document.querySelector('#image').src = "";
+
+    const prompt = document.querySelector('#prompt').value;
+    const size = document.querySelector('#size').value;
+
+    if(prompt === '') {
+        alert("Please Enter Some Text");
+        return;
+    }
+
+    generateImageRequest(prompt, size);
+}
+
+
+const generateImageRequest = async (prompt, size) => {
+    try {
+        showSpinner();
+
+        const response = await fetch('/openai/generateimage', {
+            method : "POST",
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                prompt,
+                size
+            })
+        });
+
+        if(!response.ok) {
+            removeSpinner();
+            throw new Error('That image could not be generated');
+        }
+
+        const data = await response.json()
+        const imageUrl = data.data;
+
+        document.querySelector('#image').src = imageUrl;
+
+        removeSpinner();
+        return;
+    }catch (error) {
+        document.querySelector('.msg').textContent = error
+    }
+}
+
+
+const showSpinner = () => {
+    document.querySelector('#spinner').classList.add('show');
+}
+
+const removeSpinner = () => {
+    document.querySelector('#spinner').classList.remove('show');
+}
+
+
+
+imageForm.addEventListener('submit', onSubmit)
